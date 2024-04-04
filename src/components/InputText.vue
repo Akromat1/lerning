f2ece5bf544a04da4040b009b55805d6c08f5757
Akromat1 <Akromatwot@mail.ru>
2024-04-05 01:18:08 +0300

    создал импут

---
+++ src/components/InputText.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps ({
+    placeholder: String,
+    type:String
+})
+</script>
+<template>
+    <div>
+  
+        <input type="text" v-bind:placeholder="placeholder" v-bind:type="type"  class="input-text"/>
+    </div>
+   
+</template>
+<style scoped>
+
+.input-text {
+    max-width: 350px;
+    width: 100%;
+    height: 54px;
+    padding-left: 16px;
+    padding-right: 16px;
+} 
+</style>
