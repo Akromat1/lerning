641b40af81b6029dfee14edb42e0b4e5ee27a514
Akromat1 <Akromatwot@mail.ru>
2024-04-05 00:43:53 +0300

    Создал проект по обучению

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+
+let name = ref(123)
+defineProps ({
+  msg: String,
+})
+
+</script>
+
+<template>
+  {{ msg }}
+  <span class="qq" @click="name+=1"> {{ name }}</span>
+  
+
+ 
+</template>
+
+<style scoped>
+.qq{
+  cursor: pointer;
+}
+</style>
